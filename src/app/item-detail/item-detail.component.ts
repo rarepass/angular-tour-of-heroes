ef1e0c7eb8a92e59223c310ed5be5d95c916342b
rarepath <souichi.murai@gmail.com>
2017-12-28 22:36:18 +0900

    ngforでitemdetailを表示

--- src/app/item-detail/item-detail.component.ts
+++ src/app/item-detail/item-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Item } from '../item';
+import { ItemService } from '../item.service';
 
 @Component({
   selector: 'app-item-detail',
@@ -7,7 +10,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemDetailComponent implements OnInit {
 
-  constructor() { }
+  @Input() item: Item;
+  id: number;
+  constructor(
+    private itemService: ItemService
+  ) { }
 
   ngOnInit() {
   }
